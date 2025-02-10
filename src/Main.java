a791fb5eac1cae40031f3e1a61ed8f5491fd90ed
paradox01240 <139707911+Paradox01240@users.noreply.github.com>
2025-02-10 12:18:45 +0530

    Image and Scean advance

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +19,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         try {
             // Create a new Group object to hold the root node of the scene
             Group root = new Group();
@@ -25,14 +32,73 @@ public class Main extends Application {
 
             // Set the title of the primary stage
             primaryStage.setTitle("Nice");
+            // Create a new Text object with the text "Hello, World!"
+            Text text = new Text("Hello, World!");
+
+// Set the x-coordinate of the text to 100
+            text.setX(100);
+
+// Set the y-coordinate of the text to 100
+            text.setY(100);
+
+// Set the font of the text to Verdana with a size of 50
+            text.setFont(Font.font("Verdana", 50));
+
+// Set the fill color of the text to white
+            text.setFill(Color.WHITE);
+
+            Line line = new Line();
+            line.setStartX(200);
+            line.setStartY(200);
+            line.setEndX(500);
+            line.setEndY(200);
+            line.setStroke(Color.WHITE);
+            line.setStrokeWidth(3);
+            line.setRotate(45);
+
+            // Create a rectangle object
+            Rectangle rectangle = new Rectangle();
+            rectangle.setX(100);
+            rectangle.setY(100);
+            rectangle.setWidth(100);
+            rectangle.setHeight(100);
+            rectangle.setFill(Color.RED);
+
+            // Create a polygon object
+            Polygon polygon = new Polygon();
+            polygon.getPoints().addAll(200.0, 200.0, 300.0, 300.0, 200.0, 300.0);
+            polygon.setFill(Color.BLUE);
+
+            // Create a circle object
+            Circle circle = new Circle();
+            circle.setCenterX(350);
+            circle.setCenterY(350);
+            circle.setRadius(50);
+            circle.setFill(Color.GREEN);
+
+            // Create an image object and image view
+            Image image = new Image("logo.png");
+            ImageView imageView = new ImageView(image);
+            imageView.setX(400);
+            imageView.setY(400);
+            imageView.setFitHeight(100);
+            imageView.setFitWidth(100);
+
+            // Add the text, line, rectangle, polygon, circle, and image view to the root group of the scene
+            root.getChildren().add(line);
+            root.getChildren().add(text);
+            root.getChildren().add(rectangle);
+            root.getChildren().add(polygon);
+            root.getChildren().add(circle);
+            root.getChildren().add(imageView);
 
             // Load the logo image and add it as an icon to the primary stage
-            Image image = new Image("logo.png");
-            primaryStage.getIcons().add(image);
+            Image image1 = new Image("logo.png");
+            primaryStage.getIcons().add(image1);
 
             // Set the width and height of the primary stage
-            primaryStage.setWidth(550);
-            primaryStage.setHeight(550);
+            primaryStage.setWidth(750);
+            primaryStage.setHeight(750);
 
             // Disable resizing of the primary stage
             primaryStage.setResizable(false);
@@ -47,7 +113,7 @@ public class Main extends Application {
             // Show the primary stage
             primaryStage.show();
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
     }
 }
